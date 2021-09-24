98279e3f55772362d8e7e645e3b35f2cab2ca657
Tom <tom-vautray@hotmail.fr>
2021-09-24 10:42:07 +0200

    Added testing class for HelloController

---
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+class HelloControllerTest
+{
+	@Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getHello() throws Exception
+    {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+    }
+}
