5dbd31a60783398c827de8511f4c945d049307f0
Tom <tom-vautray@hotmail.fr>
2021-09-24 09:20:46 +0200

    Project init

---
+++ src/main/java/com/example/demo/SprinCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinCourseApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SprinCourseApplication.class, args);
+	}
+}
